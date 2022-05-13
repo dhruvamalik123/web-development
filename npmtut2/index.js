59c97ccf80258cf5f4e821b678bccd71e9508641
dhruvamalik_20BCS3976 <83442881+dhruvamalik123@users.noreply.github.com>
2022-05-13 08:31:53 +0530

    commit on "master"

--- npmtut2/index.js
+++ npmtut2/index.js
@@ -1,11 +1,13 @@
+//to run this code , we have to remove the type in package.json
+
 const figlet = require("figlet");
 const colors = require("colors");
-figlet("Hello World!!", function (err, data) {
+figlet("DHRUVA", function (err, data) {
   if (err) {
     console.log("Something went wrong...");
     console.dir(err);
     return;
   }
   console.log(data.rainbow);
-  console.log("hello");
+  console.log("hello".rainbow);
 });
---
+++ npmtut2/languagedetector.js
@@ -0,0 +1,6 @@
+// const { franc, francAll } = require("franc");
+import { franc, francAll } from "franc";
+// var langs = require("langs");
+import { hasLanguage } from "langs";
+// console.log(francAll("Considerando ser essencial que os direitos humanos"));
+console.log(hasLanguage.where("3", franc("Alle menslike wesens word vry")));
