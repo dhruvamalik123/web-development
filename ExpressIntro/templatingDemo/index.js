0d85101e570c6dbe46190b2dd1dc59f306657359
dhruvamalik_20BCS3976 <83442881+dhruvamalik123@users.noreply.github.com>
2022-08-04 12:21:50 +0530

    commit on "master"

--- ExpressIntro/templatingDemo/index.js
+++ ExpressIntro/templatingDemo/index.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
 app.get("/", (req, res) => {
-  res.render("home.ejs");
+  const random = Math.floor(Math.random() * 5) + 1;
+  const name = [
+    "Good Morning",
+    "Hello There",
+    "Namaste",
+    "Guten Morgen",
+    "Hola!!!",
+  ];
+  const greetings = name[random - 1];
   console.log("all ok");
+  res.render("home.ejs", { greetings, random }); //we can remove .ejs from file name if the view engine has been set to ejs
+  //res.render("home.ejs", { random });    instead of key value pair("name variable to be returned(can be anything"):variable name in javascript) rand : random , we can just pass random also
 });
+
 app.listen(3010, () => {
   console.log("listening on port 3010");
 });
